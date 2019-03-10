3a8867658dfac33483c7d0f004f42d8c7d72ff8f
wingkwong <wingkwong.code@gmail.com>
2019-03-10 23:38:59 +0800

    finished TODO items

--- scrapers/boc.py
+++ scrapers/boc.py
@@ -14,9 +14,8 @@ import pandas as pd
 # Defining variables
 URL = 'https://www.bochk.com/services/xml/generatebranchweb.en.xml'
 COLUMNS = [
-    'code',
-    'districtName',
-    'districtCode'
+    'code', 'districtName', 'districtCode', 'name', 'latitude', 'longitude',
+    'address', 'service'
 ]
 
 # Fetching data from Bank of China (Hong Kong)
@@ -24,22 +23,56 @@ req = requests.get(URL)
 
 # Parsing XML data
 data = xmltodict.parse(req.content)
-data = list(data.values())[0] 
+data = list(data.values())[0]
 regions = list(data['Bank']['Region'])
 
-df = pd.DataFrame(regions)
+df = pd.DataFrame()
+df_code = []
+df_districtName = []
+df_districtCode = []
+df_name = []
+df_latitude = []
+df_longitude = []
+df_address = []
+df_service = []
 
-regions = pd.Series(regions)
+for region in regions:
+    df_tmp = pd.DataFrame()
+    code = region['Code']
+    districtName = region['Name'].replace('&amp;', '&')
+    districtCode = region['DistrictCode']
+    items = region['item']
 
-df['code'] = [region.get('Code') for region in regions]
-df['districtName'] = [region.get('Name') for region in regions]
-df['districtCode'] = [region.get('districtCode') for region in regions]
+    for item in items:
+        name = item['Name']
+        latitude = item['Latitude']
+        longitude = item['Longitude']
+        if 'atm' in item.keys():
+            address = item['atm']['Address']
+            services = item['atm']['Services']
+            if services is not None:
+                atm_service = services['Service']
+                if isinstance(atm_service, list):
+                    atm_service = ','.join(atm_service)
 
-###########################################################
-## TODO
-###########################################################
-## 1. Parse <item>
-###########################################################
+            df_code.append(code)
+            df_districtName.append(districtName)
+            df_districtCode.append(districtCode)
+            df_name.append(name)
+            df_latitude.append(latitude)
+            df_longitude.append(longitude)
+            df_address.append(address)
+            df_service.append(atm_service)
+
+    df_tmp['code'] = df_code
+    df_tmp['districtName'] = df_districtName
+    df_tmp['districtCode'] = df_districtCode
+    df_tmp['name'] = df_name
+    df_tmp['latitude'] = df_latitude
+    df_tmp['longitude'] = df_longitude
+    df_tmp['address'] = df_address
+    df_tmp['service'] = df_service
+    df = df.append(df_tmp, ignore_index=True)
 
 # Reordering columns
 df = df[COLUMNS]

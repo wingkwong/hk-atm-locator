3590dd9900d35665720cb77f44ce457617a692d8
wingkwong <wingkwong.code@gmail.com>
2019-03-12 22:26:16 +0800

    wrapped other routes in App

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom';
+import { Route, Layout } from 'react-router-dom';
+import App from './containers/App/App';
 import Landing from './containers/Landing/Landing';
 
 export default class Routes extends Component{
     render() {
         return (
             <div>
-                <Route path='/' component={Landing} />
+                <Route path='/' component={App}/>
+                <Route path='/map' component={Landing} />
             </div>
         )
     }

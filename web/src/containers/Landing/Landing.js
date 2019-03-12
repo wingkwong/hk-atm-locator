af22b8d5499ebf534585dda440425bfd4673d942
wingkwong <wingkwong.code@gmail.com>
2019-03-12 22:57:52 +0800

    leaflet poc

--- web/src/containers/Landing/Landing.js
+++ web/src/containers/Landing/Landing.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+const styles = {
+    mapContainer: {
+      height: '700px',
+    },
+  };
 
 class Landing extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: 22.308,
+            lng: 114.1716,
+            zoom: 13,
+        }
+    }
+
     render() {
+        const { classes } = this.props;
+        const position = [this.state.lat, this.state.lng]
         return (
-            <div>
-                Hello World!
-            </div>
+            <Map center={position} zoom={this.state.zoom} className={classes.mapContainer}>
+                <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                <Popup>
+                    Meet-up at Eaton HK 1/F at 8:00 P.M. 
+                </Popup>
+                </Marker>
+            </Map>
         )
     }
 }
@@ -24,4 +51,4 @@ const mapDispatchToProps = (dispatch) => {
 }
   
 
-export default connect(mapStateToProps, mapDispatchToProps)(Landing);
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Landing));

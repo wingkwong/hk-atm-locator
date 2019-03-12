03bc7fa5397ce55b3af3e4544d9995e2424ed861
wingkwong <wingkwong.code@gmail.com>
2019-03-12 22:25:48 +0800

    added app bar

---
+++ web/src/containers/AppNavbar/AppNavbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
+function AppNavbar(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="secondary">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            Hong Kong ATM Locator
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+AppNavbar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AppNavbar);

8591bf691b7a09d5969d28a97f9996f91183363d
wingkwong <wingkwong.code@gmail.com>
2019-03-17 12:09:30 +0800

    added redux middleware

--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import Root from './containers/Root/Root';
-import { createStore } from 'redux';
 import rootReducer from './reducers';
 import css from './static/main.css';
 
-const store = createStore(rootReducer);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const store = createStoreWithMiddleware(rootReducer);
 
 ReactDOM.render(< Root store = {
     store

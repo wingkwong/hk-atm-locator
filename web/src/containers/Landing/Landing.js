a8f9a8aeafb002bb0b9923a6fac261626651e37a
wingkwong <wingkwong.code@gmail.com>
2019-03-17 12:20:33 +0800

    added dispatch action and include ATM Marker Cluster Group

--- web/src/containers/Landing/Landing.js
+++ web/src/containers/Landing/Landing.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import ATMMarkerClusterGroup from '../../components/Leaflet/ATMMarkerClusterGroup';
 import ATMListing from '../../components/ATMListing/ATMListing';
+import HANG_SENG_DATA from '../../data/hang_seng.json';
+import HSBC_DATA from '../../data/hsbc.json';
+
+import {
+    setATMData
+} from '../../actions'
 
 const styles = {
     mapContainer: {
@@ -16,8 +23,20 @@ class Landing extends Component{
         this.state = {
             lat: 22.308,
             lng: 114.1716,
-            zoom: 13,
+            zoom: 14,
         }
+        this.initATMData();
+    }
+
+    initATMData() {
+        const hangSengATMs = HANG_SENG_DATA.data[0].Brand[0].ATM;
+        const hsbcATMs = HSBC_DATA.data[0].Brand[0].ATM;
+        const allATMs = [...hangSengATMs, ...hsbcATMs];
+        /*
+            TODO: 
+            - Data enrichment: (lat, lng for hang seng atms)
+        */
+        this.props.setATMData(allATMs);
     }
 
     render() {
@@ -26,16 +45,13 @@ class Landing extends Component{
         return (
             <React.Fragment>
                 <ATMListing/>
-                <Map center={position} zoom={this.state.zoom} className={classes.mapContainer}>
+                <Map center={position} zoom={this.state.zoom} maxZoom={16} className={classes.mapContainer}>
                     <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={position}>
-                    <Popup>
-                        Meet-up at Eaton HK 1/F at 8:00 P.M. 
-                    </Popup>
-                    </Marker>
+                    <ATMMarkerClusterGroup/>
+                    
                 </Map>
             </React.Fragment>
         )
@@ -50,7 +66,9 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-
+        setATMData: (atmData) => {
+            dispatch(setATMData(atmData));
+        }
     };
 }
   

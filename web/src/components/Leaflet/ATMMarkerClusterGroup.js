5f51f03b9bbf230435ea22942ad7fc34dc55d5b1
wingkwong <wingkwong.code@gmail.com>
2019-03-21 21:20:50 +0800

    added popup

--- web/src/components/Leaflet/ATMMarkerClusterGroup.js
+++ web/src/components/Leaflet/ATMMarkerClusterGroup.js
@@ -2,16 +2,45 @@ import React from 'react';
 import { connect } from "react-redux";
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Marker, Popup } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
-    
+    popUpContent: {
+        padding: 0,
+        '&:last-child': {
+            paddingBottom: 0
+        }
+    },
 });
 
 class ATMMarkerClusterGroup extends React.Component {
     componentDidMount() {
         
+    }
+
+    renderPopUp(data) {
+        const { classes } = this.props;
+        return (
+            <Popup>
+                <CardContent className={classes.popUpContent}>
+                    <Typography variant="h5" component="h2">
+                        { data.ATMName }
+                    </Typography>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                        { data.ATMAddress.AddressLine }
+                    </Typography>
+                    <Typography className={classes.pos} color="textSecondary">
+                        { data.BranchName }
+                    </Typography>
+                    <Typography component="p">
+                        { data.HotlineNumber }
+                    </Typography>
+                </CardContent>
+            </Popup>
+        );
     }
 
     renderMarkerClusterGroup() {
@@ -22,11 +51,23 @@ class ATMMarkerClusterGroup extends React.Component {
                     atm.map((data, idx) => {
                         const latitude = data.ATMAddress.LatitudeDescription;
                         const longitude = data.ATMAddress.LongitudeDescription;
-                        if(latitude != undefined && longitude != undefined) {
+                        if(latitude !== undefined && longitude !== undefined) {
                             return (
-                                <Marker position={[latitude, longitude]} key={idx}/>
+                                <Marker 
+                                    key= { idx }
+                                    position= { [latitude, longitude] } 
+                                    onMouseOver= { (e) => {
+                                        e.target.openPopup();
+                                    }}
+                                    onMouseOut= { (e) => {
+                                        // e.target.closePopup();
+                                    }}
+                                >
+                                { this.renderPopUp(data) }
+                                </Marker>
                             );
                         }
+                        return (null);
                     })
                 }
             </MarkerClusterGroup>

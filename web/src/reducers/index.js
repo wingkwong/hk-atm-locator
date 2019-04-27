cf6396e34f05eee5a7b9347a2dc60bb96d803f8f
Nandiheath <nandi0315@gmail.com>
2019-04-27 16:04:11 +0800

    move the filter option reducer into atm
    - to alter the filtered result directly

--- web/src/reducers/index.js
+++ web/src/reducers/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux';
 import atmDataReducer from './atmDataReducer';
 import locationReducer from './locationReducer';
 import pageReducer from './pageReducer';
-import filterOptionReducer from './filterOptionReducer';
 
 export default combineReducers({
     atm: atmDataReducer,
     location: locationReducer,
-    page: pageReducer,
-    filter: filterOptionReducer,
+    page: pageReducer
 });

853afcf1732c3f789e72a36dbe10fc3bac16a281
wingkwong <wingkwong.code@gmail.com>
2019-04-29 23:58:59 +0800

    revised services

--- web/src/constants/services.js
+++ web/src/constants/services.js
@@ -1,12 +1,51 @@
-export const SERVICE_COIN_SORT = 'coin_sort';
-export const SERVICE_FOREIGN_CURRENCY = 'foreign_currency';
-export const SERVICE_DISABLED_ACCESS = 'disabled_access';
-export const SERVICE_BILL_PAYMENT = 'bill_payment';
-export const SERVICE_CASH_WITHDRAWAL = 'cash_withdrawal';
-export const SERVICE_CASH_DEPOSIT = 'cash_deposit';
-export const SERVICE_CHEQUE_DEPOSIT = 'cheque_deposit';
+export const SERVICE_COIN_SORT = {
+  idx: 'coin_sort',
+  api_idx: 'CoinSortIndicator',
+  en: 'Coin Sort',
+  tc: ''
+};
 
+export const SERVICE_FOREIGN_CURRENCY = {
+  idx: 'foreign_currency',
+  api_idx: 'ForeignCurrencyIndicator',
+  en: 'Foreign Currency',
+  tc: ''
+};
 
+export const SERVICE_DISABLED_ACCESS = {
+  idx: 'disabled_access',
+  api_idx: 'DisabledAccessIndicator',
+  en: 'Disabled Access',
+  tc: ''
+};
+
+export const SERVICE_BILL_PAYMENT = {
+  idx: 'bill_payment',
+  api_idx: 'BillPaymentIndicator',
+  en: 'Bill Payment',
+  tc: ''
+};
+
+export const SERVICE_CASH_WITHDRAWAL = {
+  idx: 'cash_withdrawal',
+  api_idx: 'CashWithdrawalIndicator',
+  en: 'Cash Withdrawal',
+  tc: ''
+};
+
+export const SERVICE_CASH_DEPOSIT = {
+  idx: 'cash_deposit',
+  api_idx: 'BillPaymentIndicator',
+  en: 'Cash Deposit',
+  tc: ''
+};
+
+export const SERVICE_CHEQUE_DEPOSIT = {
+  idx: 'cheque_deposit',
+  api_idx: 'ChequeDepositIndicator',
+  en: 'Cheque Deposit',
+  tc: ''
+};
 
 export const SERVICES = [
   SERVICE_COIN_SORT,
@@ -16,14 +55,4 @@ export const SERVICES = [
   SERVICE_CASH_WITHDRAWAL,
   SERVICE_CASH_DEPOSIT,
   SERVICE_CHEQUE_DEPOSIT
-];
-
-export const SERVICE_NAMES = [
-  'Coin Sort',
-  'Foreign Currency',
-  'Disabled Access',
-  'Bill Payment',
-  'Cash Withdrawal',
-  'Cash Deposit',
-  'Cheque Deposit'
 ];

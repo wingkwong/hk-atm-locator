5a46630074fbf5b925baa4da31031ceb70263670
wingkwong <wingkwong.code@gmail.com>
2019-05-09 23:11:22 +0800

    merged search bar to appbar

--- web/src/containers/AppNavbar/AppNavbar.js
+++ web/src/containers/AppNavbar/AppNavbar.js
@@ -1,38 +1,179 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import MapIcon from '@material-ui/icons/Map';
+import FilterIcon from '@material-ui/icons/Tune';
+import {
+  toggleATMFilterDialog
+} from '../../actions';
 
-const styles = {
+const styles = theme => ({
   root: {
+    width: '100%',
+  },
+  grow: {
     flexGrow: 1,
   },
-  appBar: {
+  backButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: theme.palette.common.white,
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.85),
+    },
+    color: '#f50057',
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit * 3,
+      width: '40%',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
     alignItems: 'center',
-    zIndex: 1300
+    justifyContent: 'center',
+  },
+  filterIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 0,
+    top: 0
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 100,
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+});
+
+class AppNavbar extends React.Component {
+  state = {
+    
+  };
+
+  backToLandingPage = () => {
+    this.props.backToLandingPage();
   }
-};
 
-function AppNavbar(props) {
-  const { classes } = props;
+  handleViewChange = () => {
+    //TODO: Switch Listing & Map View
+  };
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="fixed" color="secondary" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit">
-            Hong Kong ATM Locator
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="secondary">
+          <Toolbar>
+            <IconButton className={classes.backButton} color="inherit" aria-label="Back to Home page" onClick={() => {this.backToLandingPage()}}>
+              <ArrowBackIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+              Hong Kong ATM Locator
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+              />
+              <div className={classes.filterIcon}>
+                <FilterIcon/>
+              </div>
+            </div>
+            <div className={classes.grow} />
+            <div >
+              <IconButton
+                onClick={this.handleViewChange}
+                color="inherit"
+              >
+                <MapIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
 AppNavbar.propTypes = {
   classes: PropTypes.object.isRequired,
+  backToLandingPage: PropTypes.func.isRequired
 };
 
-export default withStyles(styles)(AppNavbar);
+const mapStateToProps = (state, ownProps) => {
+  return {
+
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+      toggleATMFilterDialog: (open) => {
+          dispatch(toggleATMFilterDialog(open))
+      }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles, { withTheme: true })(AppNavbar));

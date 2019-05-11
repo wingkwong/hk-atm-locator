f15a30766e17b8ded421320f721582cc401af4a3
wingkwong <wingkwong.code@gmail.com>
2019-05-11 13:53:29 +0800

    removed drawer

--- web/src/components/ATMListing/ATMListing.js
+++ web/src/components/ATMListing/ATMListing.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import { withStyles } from '@material-ui/core/styles';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import ATMItem from './ATMItem';
 import ATMItemDialog from './ATMItemDialog';
-
-const drawerWidth = '100%';
 
 const styles = theme => ({
   root: {
     display: 'flex',
-  },
-  drawerPaper: {
-    width: drawerWidth,
-    position: 'relative',
-    zIndex: '1000'
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
   },
 });
 
@@ -30,18 +19,18 @@ class ATMListing extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        mobileOpen: false,
+      
     };
   }
-
-  handleDrawerToggle = () => {
-    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
-  };
 
   renderATMItems = () => {
     const allATMs = this.props.atm;
     if(allATMs === undefined) {
-      return <div>Loading</div>
+      return (
+        <ListItem>
+          <ListItemText primary={'Loading ...'} />
+        </ListItem>
+      );
     }
 
     if(allATMs.length > 0) {
@@ -54,36 +43,23 @@ class ATMListing extends React.Component {
         );
     });
     } else {
-      return <div>No ATM data</div>
+      return (
+        <ListItem>
+            <ListItemText primary={'No matching items found'} />
+        </ListItem>
+      );
     }
   };
 
   render() {
     const { classes } = this.props;
-    const drawer = (
-      <React.Fragment>
-        <List>
-            {this.renderATMItems()}
-        </List>
-        <ATMItemDialog/>
-      </React.Fragment>
-    );
 
     return (
       <div className={classes.root}>
-        <CssBaseline />
-        <nav className={classes.drawer}>
-          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-          <Drawer
-              classes={{
-                paper: classes.drawerPaper,
-              }}
-              variant="permanent"
-              open
-            >
-              {drawer}
-            </Drawer>
-        </nav>
+        <List>
+          {this.renderATMItems()}
+        </List>
+        <ATMItemDialog/>
     </div>
     );
   }

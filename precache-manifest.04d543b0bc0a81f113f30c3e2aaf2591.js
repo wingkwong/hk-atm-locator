30871b749de1e6b354e547290213b67547a43e72
wingkwong <wingkwong.code@gmail.com>
2019-05-11 18:30:27 +0800

    Updates

--- precache-manifest.04d543b0bc0a81f113f30c3e2aaf2591.js
+++ precache-manifest.04d543b0bc0a81f113f30c3e2aaf2591.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "ce2342510ea4cd263912",
+    "revision": "466140c8f645f14a6bb0",
     "url": "/hk-atm-locator/static/css/main.2811a35a.chunk.css"
   },
   {
-    "revision": "ce2342510ea4cd263912",
-    "url": "/hk-atm-locator/static/js/main.60efd6d6.chunk.js"
+    "revision": "466140c8f645f14a6bb0",
+    "url": "/hk-atm-locator/static/js/main.a366d195.chunk.js"
   },
   {
     "revision": "e7078f4e8747d91daba4",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/hk-atm-locator/static/media/you_are_here.e985b822.png"
   },
   {
-    "revision": "7fd28d04142259a31f2348941731b04b",
+    "revision": "06b26515bdfe0a6a452ada29b63bc4c9",
     "url": "/hk-atm-locator/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hk-atm-locator/precache-manifest.6987607f095d7e7f39d17815de394a64.js"
+  "/hk-atm-locator/precache-manifest.04d543b0bc0a81f113f30c3e2aaf2591.js"
 );
 
 workbox.clientsClaim();

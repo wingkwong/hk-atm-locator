c017567d5f3695657d860b5a9a8590e5f3b92206
wingkwong <wingkwong.code@gmail.com>
2019-05-14 22:56:52 +0800

    added jetco ref data

---
+++ transformer/reference/jetco_banks.js
@@ -0,0 +1,137 @@
+const bochk = {
+    idx: 'bochk',
+    en: 'Bank of China (Hong Kong) Limited',
+    zh: ''
+}
+
+const bankcomm = {
+    idx: 'bankcomm',
+    en: 'Bank of Communications Co., Ltd.',
+    zh: ''
+}
+
+const cncbinternational = {
+    idx: 'cncbinternational',
+    en: 'China CITIC Bank International Limited',
+    zh: ''
+}
+
+const asiaccb = {
+    idx: 'asiaccb',
+    en: 'China Construction Bank (Asia) Corporation Limited',
+    zh: ''
+}
+
+const cmbchina = {
+    idx: 'cmbchina',
+    en: 'China Merchants Bank Hong Kong Branch',
+    zh: ''
+}
+
+const chiyubank = {
+    idx: 'chiyubank',
+    en: 'Chiyu Banking Corporation Limited',
+    zh: ''
+}
+
+const chonghingbank = {
+    idx: 'chonghingbank',
+    en: 'Chong Hing Bank Limited',
+    zh: ''
+}
+
+const citibank = {
+    idx: 'citibank',
+    en: 'Citibank (Hong Kong) Limited',
+    zh: ''
+}
+
+const cmbwinglungbank = {
+    idx: 'cmbwinglungbank',
+    en: 'CMB Wing Lung Bank Limited',
+    zh: ''
+}
+
+const dahsing = {
+    idx: 'dahsing',
+    en: 'Dah Sing Bank, Limited',
+    zh: ''
+}
+
+const dbs = {
+    idx: 'dbs',
+    en: 'DBS Bank (Hong Kong) Limited',
+    zh: ''
+}
+
+const fubonbank = {
+    idx: 'fubonbank',
+    en: 'Fubon Bank (Hong Kong) Limited',
+    zh: ''
+}
+
+const icbcasia = {
+    idx: 'icbcasia',
+    en: 'Industrial and Commercial Bank of China (Asia) Limited',
+    zh: ''
+}
+
+const ocbcwhhk = {
+    idx: 'ocbcwhhk',
+    en: 'OCBC Wing Hang Bank Limited',
+    zh: ''
+}
+
+const publicbank = {
+    idx: 'publicbank',
+    en: 'Public Bank (Hong Kong) Limited',
+    zh: ''
+}
+
+const shacombank = {
+    idx: 'shacombank',
+    en: 'Shanghai Commercial Bank Limited',
+    zh: ''
+}
+
+const nanyang = {
+    idx: 'shacombank',
+    en: 'Nanyang Commercial Bank Limited',
+    zh: ''
+}
+
+const standardchartered = {
+    idx: 'standardchartered',
+    en: 'Standard Chartered Bank (Hong Kong) Limited',
+    zh: ''
+}
+
+const hkbea = {
+    idx: 'hkbea',
+    en: 'The Bank of East Asia, Limited',
+    zh: ''
+}
+
+module.exports = {
+    jetco : [
+        bochk,
+        bankcomm,
+        cncbinternational,
+        asiaccb,
+        cmbchina,
+        chiyubank,
+        chonghingbank,
+        citibank,
+        cmbwinglungbank,
+        dahsing,
+        dbs,
+        fubonbank,
+        icbcasia,
+        ocbcwhhk,
+        nanyang,
+        publicbank,
+        shacombank,
+        standardchartered,
+        hkbea
+    ]
+}

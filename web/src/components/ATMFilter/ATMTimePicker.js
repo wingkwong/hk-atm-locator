8a1755507a3076d2691543510e7bd447b53657e7
wingkwong <wingkwong.code@gmail.com>
2019-05-18 18:49:20 +0800

    Create ATMTimePicker.js

---
+++ web/src/components/ATMFilter/ATMTimePicker.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginTop: '20px'
+  },
+});
+
+class ATMTimePicker extends React.Component {
+  render() {
+    const { classes, label, defaultValue } = this.props;
+    return (
+      <form className={classes.container} noValidate>
+        <TextField
+          id="time"
+          label={label}
+          type="time"
+          defaultValue={defaultValue}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          inputProps={{
+            step: 300,
+          }}
+        />
+      </form>
+    );
+  }
+}
+
+ATMTimePicker.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ATMTimePicker);

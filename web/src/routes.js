ce597395ecd33f5fae0ce75cfe5aa94a1954f1ba
wingkwong <wingkwong.code@gmail.com>
2019-06-12 19:14:03 +0800

    updated route

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ATMLandingContainer from './containers/Landing/ATMLandingContainer';
+import ATMDetailContainer from './containers/ATMDetailContainer/ATMDetailContainer';
 import ATM404Container from './containers/ATM404Container/ATM404Container';
 
 export default class Routes extends Component{
@@ -8,6 +9,7 @@ export default class Routes extends Component{
         return (
             <Switch>
                 <Route exact path='/' component={ATMLandingContainer}/>
+                <Route path='/atm/:id' component={ATMDetailContainer}/>
                 <Route component={ATM404Container} />
             </Switch>
         )
